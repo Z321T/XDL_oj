5578553c3105f5772f342c218deb1c13d78a50b2
Z321T <fzc19942496944@outlook.com>
2024-02-10 10:46:56 +0800

    教师与管理员界面的home界面的修改
    让右上角可以显示登录信息

--- administrator_app/views.py
+++ administrator_app/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_administrator(request):
-
-    return render(request, 'home_administrator.html')
+    dropdown_menu1 = {
+        'user_id': request.session.get('user_id'),
+    }
+    return render(request, 'home_administrator.html', {'dropdown_menu1': dropdown_menu1})
--- teacher_app/views.py
+++ teacher_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home_teacher(request):
-    return render(request, 'home_teacher.html')
+    dropdown_menu1 = {
+        'user_id': request.session.get('user_id'),
+    }
+    return render(request, 'home_teacher.html', {'dropdown_menu1': dropdown_menu1})
