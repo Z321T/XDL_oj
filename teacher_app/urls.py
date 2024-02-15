db1aaaf54f558b40df10dfb0761cf33d206d1ae6
Z321T <fzc19942496944@outlook.com>
2024-02-15 17:22:04 +0800

    创建班级的实现
    不过没有真实测试

--- teacher_app/urls.py
+++ teacher_app/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from .views import home_teacher
-from .views import test_teacher
-from .views import profile_teacher
-from .views import notice_teacher
-from .views import repository_teacher
-from .views import class_teacher
+from .views import home_teacher, test_teacher, profile_teacher, notice_teacher, repository_teacher, class_teacher
+from .views import create_class
 
 from django.contrib.staticfiles.views import serve
 
@@ -20,6 +16,7 @@ urlpatterns = [
     path('notice/', notice_teacher, name='notice_teacher'),
     path('repository/', repository_teacher, name='repository_teacher'),
     path('class/', class_teacher, name='class_teacher'),
+    path('class/create/', create_class, name='create_class'),
 
 
     path('static/<path:path>', serve),
