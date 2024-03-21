67742847a5b57b078d6235ff51851002a2ee1db0
HEPUXI <107572072+HEPUXI@users.noreply.github.com>
2024-03-21 15:16:37 +0800

    所有空白改好了，开始改框体样式
    提交报告还是有问题

--- temp.cpp
+++ temp.cpp
@@ -1,1 +1,36 @@
-cout << 2 << endl;
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    if (num <= 3) {
+        return true;
+    }
+
+    if (num % 2 == 0 || num % 3 == 0) {
+        return false;
+    }
+
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    std::cout << "Prime numbers between 1 and 20 are: ";
+
+    for (int num = 1; num <= 20; ++num) {
+        if (isPrime(num)) {
+            std::cout << num << " ";
+        }
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
