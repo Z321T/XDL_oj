f22c4347a9bdadd7fb2468489c03b3729f9d5054
Z321T <fzc19942496944@outlook.com>
2024-04-08 00:19:41 +0800

    对项目部分代码的修改

--- CodeBERT_app/views.py
+++ CodeBERT_app/views.py
@@ -15,29 +15,6 @@ tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
 model = AutoModel.from_pretrained("microsoft/codebert-base")
 
 
-# 分析练习与考试代码
-# def analyze_code(student, code, types, question_id=None):
-#     # 分词
-#     tokenized_code = tokenizer.tokenize(code)
-#     features = []
-#     for i in range(0, len(tokenized_code), 512):
-#         inputs = tokenizer(tokenized_code[i:i+512], return_tensors="pt", padding=True, truncation=True, max_length=512)
-#         # 获取特征值
-#         with torch.no_grad():
-#             feature = model(**inputs).last_hidden_state.mean(dim=1)
-#             features.append(feature)
-#     # 连接所有特征值
-#     concatenated_features = torch.cat(features, dim=0)
-#     feature_as_json = json.dumps(concatenated_features.tolist())
-#
-#     if types == 'exercise':
-#         question = ExerciseQuestion.objects.get(id=question_id)
-#         CodeFeature.objects.create(student=student, exercise_question=question, feature=feature_as_json)
-#     elif types == 'exam':
-#         question = ExamQuestion.objects.get(id=question_id)
-#         CodeFeature.objects.create(student=student, exam_question=question, feature=feature_as_json)
-
-
 # 分析程序设计题代码
 def analyze_programming_code(student, code, question_id):
     # 代码分词
@@ -214,3 +191,25 @@ def score_report(student, document, programmingexercise_id):
         programming_question_id=programmingexercise_id,
         defaults={'standard_score': total_score}
     )
+
+# 分析练习与考试代码
+# def analyze_code(student, code, types, question_id=None):
+#     # 分词
+#     tokenized_code = tokenizer.tokenize(code)
+#     features = []
+#     for i in range(0, len(tokenized_code), 512):
+#         inputs = tokenizer(tokenized_code[i:i+512], return_tensors="pt", padding=True, truncation=True, max_length=512)
+#         # 获取特征值
+#         with torch.no_grad():
+#             feature = model(**inputs).last_hidden_state.mean(dim=1)
+#             features.append(feature)
+#     # 连接所有特征值
+#     concatenated_features = torch.cat(features, dim=0)
+#     feature_as_json = json.dumps(concatenated_features.tolist())
+#
+#     if types == 'exercise':
+#         question = ExerciseQuestion.objects.get(id=question_id)
+#         CodeFeature.objects.create(student=student, exercise_question=question, feature=feature_as_json)
+#     elif types == 'exam':
+#         question = ExamQuestion.objects.get(id=question_id)
+#         CodeFeature.objects.create(student=student, exam_question=question, feature=feature_as_json)
